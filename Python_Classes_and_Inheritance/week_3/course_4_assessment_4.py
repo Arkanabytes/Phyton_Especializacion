9bb86ae48bb55d805bb2e321313dceb526924b6d
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-28 21:34:07 +0200

    Update course_4_assessment_4.py

--- Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
+++ Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
@@ -90,7 +90,7 @@ for x in sport:
         ppl_play[x] = 1
         
         
-8.-Provided is a buggy for loop that tries to accumulate some values out of some dictionaries. Insert a try/except 
+#8.-Provided is a buggy for loop that tries to accumulate some values out of some dictionaries. Insert a try/except 
 so that the code passes. If the key is not there, initialize it in the dictionary and set the value to zero.
 
 di = [{"Puppies": 17, 'Kittens': 9, "Birds": 23, 'Fish': 90, "Hamsters": 49}, {"Puppies": 23, "Birds": 29, "Fish": 20, "Mice": 20, "Snakes": 7}, {"Fish": 203, "Hamsters": 93, "Snakes": 25, "Kittens": 89}, {"Birds": 20, "Puppies": 90, "Snakes": 21, "Fish": 10, "Kittens": 67}]

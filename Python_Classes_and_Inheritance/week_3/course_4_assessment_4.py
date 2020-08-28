95246168ae0888c65594204bf3955f9ed1e75dc5
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-28 21:35:48 +0200

    .

--- Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
+++ Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
@@ -1,3 +1,5 @@
+#Arkanabytes
+
 #1.-The code below takes the list of country, country, and searches to see if it is in the dictionary gold which shows some countries who won gold during the Olympics.
 #However, this code currently does not work. Correctly add try/except clause in the code so that it will correctly populate the list, country_gold, with either the 
 #number of golds won or the string “Did not get gold”.
@@ -13,7 +15,7 @@ for x in country:
         country_gold.append("Did not get gold")
         
         
- #2.-Provided is a buggy for loop that tries to accumulate some values out of some dictionaries. Insert a try/except so that the code passes
+#2.-Provided is a buggy for loop that tries to accumulate some values out of some dictionaries. Insert a try/except so that the code passes
  di = [{"Puppies": 17, 'Kittens': 9, "Birds": 23, 'Fish': 90, "Hamsters": 49}, {"Puppies": 23, "Birds": 29, "Fish": 20, "Mice": 20, "Snakes": 7}, {"Fish": 203, "Hamsters": 93, "Snakes": 25, "Kittens": 89}, {"Birds": 20, "Puppies": 90, "Snakes": 21, "Fish": 10, "Kittens": 67}]
 total = 0
 for diction in di:

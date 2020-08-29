d4608b6b6471bd49c25cd61a46c2a2212e593edb
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-29 16:01:04 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
+++ Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
@@ -1,3 +1,5 @@
+#Arkanabytes
+
 #Create a function called mult that has two parameters, the first is required and should be an integer, the second is an optional parameter that can either be a number or a string but whose default is 6. The function should return the first parameter multiplied by the second.
 lol = 6
 def mult(x,y = lol):

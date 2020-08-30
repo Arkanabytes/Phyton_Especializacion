890a0e13c08c3a153dc8490a4e890696cacd20a3
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-30 22:09:38 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
+++ Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
@@ -31,8 +31,12 @@ def sum(intx, intz=5):
 def test(x, abool = True, dict1 = {2:3, 4:5, 6:8}):
     return abool and dict1.get(x)
 
-#Write a function called checkingIfIn that takes three parameters. The first is a required parameter, which should be a string. The second is an optional parameter called direction with a default value of True. The third is an optional parameter called d that has a default value of {'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}. Write the function checkingIfIn so that when the second parameter is True, it checks to see if the first parameter is a key in the third parameter; if it is, return True, otherwise return False.
-#But if the second paramter is False, then the function should check to see if the first parameter is not a key of the third. If it’s not, the function should return True in this case, and if it is, it should return False.
+#Write a function called checkingIfIn that takes three parameters. The first is a required parameter, which should be a string. The second is an optional 
+#parameter called direction with a default value of True. The third is an optional parameter called d that has a default value of {'apple': 2, 'pear': 1, 'fruit': 19, 
+#'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}. Write the function checkingIfIn so that when the second parameter is True, it checks to see if the first 
+#parameter is a key in the third parameter; if it is, return True, otherwise return False.
+#But if the second paramter is False, then the function should check to see if the first parameter is not a key of the third. If it’s not, the function should 
+#return True in this case, and if it is, it should return False.
 
 def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}):
     if direction == True:

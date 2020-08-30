c170f990380ed3528cd1a9bb547b1b09c42e5c7a
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-30 22:13:01 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_6.py
+++ Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_6.py
@@ -17,6 +17,7 @@ print(sublist(x))
 
 #Write a function called check_nums that takes a list as its parameter, and contains a while loop that only stops once the element of the list is the number 7. What
 #is returned is a list of all of the numbers up until it reaches 7.
+
 def check_nums(x):
     sub = []
     x = (num for num in x) 

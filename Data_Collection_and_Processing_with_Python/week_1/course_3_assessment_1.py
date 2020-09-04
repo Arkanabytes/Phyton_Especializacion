f141f28d67e30069792c69806ae3337f482ebdee
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-04 22:33:17 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
+++ Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
@@ -26,7 +26,9 @@ orange = 'orange' in lst[0]
 L = [[5, 8, 7], ['hello', 'hi', 'hola'], [6.6, 1.54, 3.99], ['small', 'large']]
  
 # Test if 'hola' is in the list L. Save to variable name test1
+
 test1 = 'hola' in L
+
 # Test if [5, 8, 7] is in the list L. Save to variable name test2
 test2 = [5, 8, 7] in L
 # Test if 6.6 is in the third element of list L. Save to variable name test3

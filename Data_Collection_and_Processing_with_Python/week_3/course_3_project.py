9a54265744ca2626cde1774cd42ee9f6fe7fb62c
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-05 23:34:43 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
+++ Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
@@ -41,7 +41,6 @@ extract_movie_titles(get_movies_from_tastedive("Black Panther"))
 import requests_with_caching
 import json
 
-
 def get_movies_from_tastedive(title):
     endpoint = 'https://tastedive.com/api/similar'
     param = {}
@@ -75,7 +74,6 @@ get_related_titles(["Black Panther", "Captain Marvel"])
 import requests_with_caching
 import json
 
-
 def get_movie_data(title):
     endpoint = 'http://www.omdbapi.com/'
     param = {}
@@ -85,14 +83,12 @@ def get_movie_data(title):
 
     return json.loads(this_page_cache.text)
 
-
 get_movie_data("Venom")
 get_movie_data("Baby Mama")
 
 ##Please copy the completed function from above into this active code window. Now write a function called get_movie_rating. It takes an OMDB dictionary result for one movie and extracts the Rotten Tomatoes rating as an integer. For example, if given the OMDB dictionary for “Black Panther”, it would return 97. If there is no Rotten Tomatoes rating, return 0.
 import requests_with_caching
 import json
-
 
 def get_movie_data(title):
     endpoint = 'http://www.omdbapi.com/'
@@ -108,7 +104,6 @@ def get_movie_rating(dic):
         if dic_item['Source'] == 'Rotten Tomatoes':
             return int(dic_item['Value'][:-1])
     return 0
-
 
 get_movie_rating(get_movie_data("Deadpool 2"))
 

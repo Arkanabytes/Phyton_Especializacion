98c259a86419132affa0f96a92c9f0074e16eb4d
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-07 23:35:38 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
+++ Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
@@ -1,6 +1,7 @@
 #Arkanabytes
 
 #Sort the following string alphabetically, from z to a, and assign it to the variable sorted_letters.
+
 letters = "alwnfiwaksuezlaeiajsdl"
 sorted_letters = sorted(letters, reverse = True)
 

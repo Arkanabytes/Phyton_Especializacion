8b570db01deb0bf8029e6e2643dd1d0c4a8646a2
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-08 21:11:04 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
+++ Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
@@ -8,6 +8,7 @@ print(map_testing)
 
 #Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that 
 #begin with B.
+
 countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
 b_countries = filter(lambda c : "B" in c,countries)
 

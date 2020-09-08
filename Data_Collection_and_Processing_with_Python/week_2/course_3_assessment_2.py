7ece7b7589438e4b96c6eba3093037bd20e4b182
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-08 21:10:28 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
+++ Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
@@ -1,6 +1,7 @@
 #Arkanabytes
 
 #Write code to assign to the variable map_testing all the elements in lst_check while adding the string “Fruit: ” to the beginning of each element using mapping.
+
 lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
 map_testing = map(lambda word: "Fruit: " + word,lst_check)
 print(map_testing)

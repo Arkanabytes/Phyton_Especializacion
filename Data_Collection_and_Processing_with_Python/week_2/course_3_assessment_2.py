056b3da7559d5db6f20b2d77558b1fb6a27465b0
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-08 21:13:04 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
+++ Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
@@ -26,6 +26,7 @@ lst2 =  [word*2  for word in lst]
 
 #Below, we have provided a list of tuples that contain students’ names and their final grades in PYTHON 101. Using list comprehension, create a new list passed that 
 #contains the names of students who passed the class (had a final grade of 70 or greater).
+
 students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond', 50), ('Sue', 75)]
 passed = [key[0]  for key in students if key[1] >= 70]
 print(list(passed))

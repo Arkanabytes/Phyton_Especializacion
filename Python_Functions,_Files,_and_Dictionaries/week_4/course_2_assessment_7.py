2f2a15f8a73e747aa1b2fcc26020f4d182f577cf
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-10 22:43:56 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
+++ Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_7.py
@@ -70,6 +70,7 @@ c_false = checkingIfIn('peas')
 
 # Call the fucntion so that it returns True and assign it to the variable c_true
 c_true = checkingIfIn('apples', False, {'carrots': 1, 'peas': 9, 'potatos': 8, 'corn': 32, 'beans': 1})
+
 # Call the function so that the value of fruit is assigned to the variable fruit_ans
 fruit_ans= checkingIfIn('fruit')
 # Call the function using the first and third parameter so that the value 8 is assigned to the variable param_check

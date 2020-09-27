461abe448914045a559dbe15a7c9df56269ce637
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-27 22:06:49 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
+++ Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
@@ -1,5 +1,4 @@
 #Arkanabytes
-
 # 1. The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
  
 nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]

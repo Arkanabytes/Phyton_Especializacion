a824e65dcdb7560d3febd8dcbf155475aee972bc
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-27 22:09:23 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
+++ Data_Collection_and_Processing_with_Python/week_1/course_3_assessment_1.py
@@ -94,7 +94,6 @@ third = [i[2] for i in l_of_l]
 # 9. Given below is a list of lists of athletes.
 # Create a list, t, that saves only the athlete’s name if it contains the letter “t”.
 # If it does not contain the letter “t”, save the athlete name into list other.
- 
 athletes = [
     ['Phelps', 'Lochte', 'Schooling', 'Ledecky', 'Franklin'],
     ['Felix', 'Bolt', 'Gardner', 'Eaton'],

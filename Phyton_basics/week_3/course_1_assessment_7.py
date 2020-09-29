cfc3342d564c219bb4508d9cce060629a5c9c998
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-29 22:06:38 +0200

    .

--- Phyton_basics/week_3/course_1_assessment_7.py
+++ Phyton_basics/week_3/course_1_assessment_7.py
@@ -14,7 +14,6 @@ for num in mylist:
 print(num_rainy_months)
 
 #The variable sentence stores a string. Write code to determine how many words in sentence start and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
-
 sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
 words = sentence.split()
 print(words)

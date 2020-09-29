780f76ce27d171f30cce44f8599c7b06cba49508
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-29 22:07:58 +0200

    .

--- Phyton_basics/week_3/course_1_assessment_7.py
+++ Phyton_basics/week_3/course_1_assessment_7.py
@@ -68,8 +68,6 @@ for word in words:
         
 print(num_a_or_e)
         
-
-
 #Write code that will count the number of vowels in the sentence s and assign the result to the variable num_vowels. For this problem, vowels are only a, e, i, o, and u. Hint: use the in operator with vowels.
 s = "singing in the rain and playing in the rain are two entirely different situations but both can be fun"
 vowels = ['a','e','i','o','u']

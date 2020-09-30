47bc13dee4be1aac5de82251c202afc03bbc1fea
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-30 21:14:34 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
+++ Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
@@ -6,8 +6,7 @@ output = nested[1][2]
 print(output)
  
 # 2. Below, a list of lists is provided. Use in and not in tests to create variables with Boolean values.
-# See comments for further instructions.
- 
+# See comments for further instructions. 
 lst = [['apple', 'orange', 'banana'], [5, 6, 7, 8, 9.9, 10], ['green', 'yellow', 'purple', 'red']]
  
 #Test to see if 'yellow' is in the third list of lst. Save to variable ``yellow``

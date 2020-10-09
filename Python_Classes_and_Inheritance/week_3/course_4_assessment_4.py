b4a1683d5cd24baec93d0626863f6823ab672b65
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-09 23:35:40 +0200

    .

--- Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
+++ Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
@@ -54,7 +54,6 @@ for num in lst:
 
 #5.-Write code so that the buggy code provided works using a try/except. When the codes does 
 #not work in the try, have it append to the list attempt the string “Error”
-
 full_lst = ["ab", 'cde', 'fgh', 'i', 'jkml', 'nop', 'qr', 's', 'tv', 'wxy', 'z']
 
 attempt = []

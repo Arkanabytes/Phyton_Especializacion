54986993ffb8331359c349ebc84fc8536afb33ea
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-09 23:34:23 +0200

    .

--- Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
+++ Python_Classes_and_Inheritance/week_3/course_4_assessment_4.py
@@ -79,7 +79,6 @@ for c in conts:
 
 #7.-The buggy code below prints out the value of the sport in the list sport. Use try/except so 
 #that the code will run properly. If the sport is not in the dictionary, ppl_play, add it in with the value of 1.
-
 sport = ["hockey", "basketball", "soccer", "tennis", "football", "baseball"]
 
 ppl_play = {"hockey":4, "soccer": 10, "football": 15, "tennis": 8}

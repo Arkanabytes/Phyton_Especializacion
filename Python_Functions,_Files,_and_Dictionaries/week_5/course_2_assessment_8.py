d139d931a4e0726a9f6ad9cf591a5a41b165ccbc
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-10 22:22:57 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
+++ Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
@@ -5,7 +5,6 @@ letters = "alwnfiwaksuezlaeiajsdl"
 sorted_letters = sorted(letters, reverse = True)
 
 #Sort the list below, animals, into alphabetical order, a-z. Save the new list as animals_sorted.
-
 animals = ['elephant', 'cat', 'moose', 'antelope', 'elk', 'rabbit', 'zebra', 'yak', 'salamander', 'deer', 'otter', 'minx', 'giraffe', 'goat', 'cow', 'tiger', 'bear']
 animals_sorted = sorted(animals)
 

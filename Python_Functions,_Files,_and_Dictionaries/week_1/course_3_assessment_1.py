b42aedf97c28184aa62d5c7a0f51fa227ebc5534
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-13 20:11:50 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
+++ Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
@@ -66,7 +66,6 @@ london_gold = nested_d['London']['Great Britain']
  
 # 6. Below, we have provided a nested dictionary.
 # Index into the dictionary to create variables that we have listed in the ActiveCode window.
- 
 sports = {'swimming': ['butterfly', 'breaststroke', 'backstroke', 'freestyle'], 'diving': ['springboard', 'platform', 'synchronized'], 'track': ['sprint', 'distance', 'jumps', 'throws'], 'gymnastics': {'women':['vault', 'floor', 'uneven bars', 'balance beam'], 'men': ['vault', 'parallel bars', 'floor', 'rings']}}
  
 # Assign the string 'backstroke' to the name v1

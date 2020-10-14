64f96020f6451d5f29c4ffa1bd6bbdb16c29afb8
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-15 00:00:15 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
+++ Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
@@ -79,8 +79,7 @@ v3 = sports['gymnastics']['women']
 v4 = sports['gymnastics']['men'][3]
 print(v4)
  
-# 7. Given the dictionary, nested_d,
-# save the medal count for the USA from all three Olympics in the dictionary to the list US_count.
+# 7. Given the dictionary, nested_d,save the medal count for the USA from all three Olympics in the dictionary to the list US_count.
 nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
 US_count = []
  

73d3456168f3f5bbfbc17e30b1376f7600645933
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-15 00:00:38 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
+++ Python_Functions,_Files,_and_Dictionaries/week_1/course_3_assessment_1.py
@@ -93,8 +93,7 @@ l_of_l = [['purple', 'mauve', 'blue'], ['red', 'maroon', 'blood orange', 'crimso
  
 third = [i[2] for i in l_of_l]
  
-# 9. Given below is a list of lists of athletes.
-# Create a list, t, that saves only the athlete’s name if it contains the letter “t”.
+# 9. Given below is a list of lists of athletes.Create a list, t, that saves only the athlete’s name if it contains the letter “t”.
 # If it does not contain the letter “t”, save the athlete name into list other.
 athletes = [
     ['Phelps', 'Lochte', 'Schooling', 'Ledecky', 'Franklin'],

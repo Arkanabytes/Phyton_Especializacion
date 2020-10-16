c9b524a3e243467dc9f3e1467b982a184af19634
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-16 23:17:14 +0200

    .

--- Phyton_basics/week_2/course_1_assessment_6.py
+++ Phyton_basics/week_2/course_1_assessment_6.py
@@ -70,7 +70,6 @@ nums=list(range(68))
 
 #8.-Write code to create a list of word lengths for the words in original_str using the accumulation pattern and assign the answer to a variable num_words_list. 
 #(You should use the len function).
-
 original_str = "The quick brown rhino jumped over the extremely lazy fox"
 # Answer
 words=original_str.split()

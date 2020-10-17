043fc46793d5a2cf84096dc90f158b482e40a618
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-17 23:23:45 +0200

    .

--- Phyton_basics/week_2/course_1_assessment_6.py
+++ Phyton_basics/week_2/course_1_assessment_6.py
@@ -93,7 +93,7 @@ print(lett)
 #Write a program that uses the turtle module and a for loop to draw something. It doesn’t have to be complicated, but draw something different than we have done 
 #in the past. (Hint: if you are drawing something complicated, it could get tedious to watch it draw over and over. Try setting .speed(10) for the turtle to draw
 #fast, or .speed(0) for it to draw super fast with no animation.)
-
+#Answer
 import turtle
 wn = turtle.Screen()
 m = turtle.Turtle()

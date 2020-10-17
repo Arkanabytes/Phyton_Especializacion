98d29f7eabd3d9de2b01854cff410c32cb05a137
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-17 23:24:24 +0200

    .

--- Phyton_basics/week_2/course_1_assessment_6.py
+++ Phyton_basics/week_2/course_1_assessment_6.py
@@ -25,7 +25,6 @@ for element in str_list:
     
 #4.-Write code to count the number of characters in original_str using the accumulation pattern and assign the answer to a variable num_chars. 
 Do NOT use the len function to solve the problem (if you use it while you are working on this problem, comment it out afterward!)
-
 original_str = "The quick brown rhino jumped over the extremely lazy fox."
 # Answer:
 num_chars=0

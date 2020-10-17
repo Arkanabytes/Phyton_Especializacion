de7b00658d4367d5492f143a7be4719bc9b1c233
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-17 23:23:59 +0200

    .

--- Phyton_basics/week_2/course_1_assessment_6.py
+++ Phyton_basics/week_2/course_1_assessment_6.py
@@ -4,7 +4,8 @@
 my_str = "MICHIGAN"
 for i in my_str:
     print(i)
-    
+ 
+
 #2.-Write one for loop to print out each element of the list several_things. Then, write another for loop to print out the TYPE of each element of 
 #the list several_things. To complete this problem you should have written two different for loops, each of which iterates over the list several_things, 
 #but each of those 2 for loops should have a different result.

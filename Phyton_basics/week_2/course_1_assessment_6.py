6dc151f124e6f8c8ee8af2b7589c23974885b51f
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-17 23:26:39 +0200

    .

--- Phyton_basics/week_2/course_1_assessment_6.py
+++ Phyton_basics/week_2/course_1_assessment_6.py
@@ -37,7 +37,6 @@ print(num_chars)
 #5.-addition_str is a string with a list of numbers separated by the + sign. Write code that uses the accumulation pattern to take the sum of 
 #all of the numbers and assigns it to sum_val (an integer). (You should use the .split("+") function to split by "+" and int() to cast to an integer).
 addition_str = "2+5+10+20"
-
 # Answer
 addition_str = "2+5+10+20"
 numbers=addition_str.split("+")

d7d21efee9681565f72f280039ff461bb2352071
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-18 22:34:52 +0200

    .

--- Phyton_basics/week_3/course_1_assessment_7.py
+++ Phyton_basics/week_3/course_1_assessment_7.py
@@ -45,7 +45,6 @@ print(acc_num)
 # Note 1: be sure to not double-count words that contain both an a and an e.
 # HINT 1: Use the in operator.
 # HINT 2: You can either use or or elif.
-
 # Hard-coded answers will receive no credit.
 sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems."
 words = sentence.split()

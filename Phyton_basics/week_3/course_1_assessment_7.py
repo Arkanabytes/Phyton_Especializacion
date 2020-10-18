67c8b9173a0f8c8dee262d0d69c427e198f2bbbd
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-18 22:34:34 +0200

    .

--- Phyton_basics/week_3/course_1_assessment_7.py
+++ Phyton_basics/week_3/course_1_assessment_7.py
@@ -44,7 +44,6 @@ print(acc_num)
 #Write code that counts the number of words in sentence that contain either an “a” or an “e”. Store the result in the variable num_a_or_e.
 # Note 1: be sure to not double-count words that contain both an a and an e.
 # HINT 1: Use the in operator.
-
 # HINT 2: You can either use or or elif.
 
 # Hard-coded answers will receive no credit.

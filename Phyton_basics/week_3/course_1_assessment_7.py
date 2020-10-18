b4244a79f8caecb09a38997768df4e7428aee600
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-18 22:32:01 +0200

    .

--- Phyton_basics/week_3/course_1_assessment_7.py
+++ Phyton_basics/week_3/course_1_assessment_7.py
@@ -28,7 +28,6 @@ print(same_letter_count)
 
 
 #Write code to count the number of strings in list items that have the character w in it. Assign that number to the variable acc_num.
-
 #HINT 1: Use the accumulation pattern!
 
 #HINT 2: the in operator checks whether a substring is present in a string.

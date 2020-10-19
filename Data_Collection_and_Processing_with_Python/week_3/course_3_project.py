80db5e2957d2eef7125adb4509b20e00a0f2f9b4
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-19 23:33:54 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
+++ Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
@@ -102,6 +102,7 @@ def get_movie_rating(dic):
 
 get_movie_rating(get_movie_data("Deadpool 2"))
 
+
 ###Now, you’ll put it all together. Don’t forget to copy all of the functions that you have previously defined into this code window. Define a function get_sorted_recommendations.
 #It takes a list of movie titles as an input. It returns a sorted list of related movie titles as output, up to five related movies for each input movie title. The movies 
 #should be sorted in descending order by their Rotten Tomatoes rating, as returned by the get_movie_rating function. Break ties in reverse alphabetic order, so that ‘Yahşi Batı’ 

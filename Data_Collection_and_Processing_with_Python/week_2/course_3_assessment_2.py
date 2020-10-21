e861cd92b8e5504e5e85eda3569453b0477133c6
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-21 22:25:11 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
+++ Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
@@ -30,6 +30,7 @@ students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond'
 passed = [key[0]  for key in students if key[1] >= 70]
 print(list(passed))
 
+
 #Write code using zip and filter so that these lists (l1 and l2) are combined into one big list and assigned to the variable opposites if they are both longer than 3 characters each.
 l1 = ['left', 'up', 'front']
 l2 = ['right', 'down', 'back']

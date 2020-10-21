fcdf199b0f06805182f507798e971b5c8ea74213
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-21 22:33:50 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
+++ Data_Collection_and_Processing_with_Python/week_3/course_3_project.py
@@ -147,8 +147,7 @@ def get_movie_data(title):
     this_page_cache = requests_with_caching.get(endpoint, params=param)
     return json.loads(this_page_cache.text)
 
-# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
-# get_movie_rating(get_movie_data("Deadpool 2"))
+# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages get_movie_rating(get_movie_data("Deadpool 2"))
 def get_movie_rating(data):
     rating = 0
     for i in data['Ratings']:

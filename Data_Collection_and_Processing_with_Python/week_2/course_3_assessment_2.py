2c3e2f16bfbef0cde204605fc9deafc72820aa3d
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-21 22:24:25 +0200

    .

--- Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
+++ Data_Collection_and_Processing_with_Python/week_2/course_3_assessment_2.py
@@ -5,6 +5,7 @@ lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peac
 map_testing = map(lambda word: "Fruit: " + word,lst_check)
 print(map_testing)
 
+
 #Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that 
 #begin with B.
 countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']

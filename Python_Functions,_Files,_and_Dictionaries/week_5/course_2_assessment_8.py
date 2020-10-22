c65d22576ac87e778f8bc97c77f264ac1d99dbba
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-22 23:08:44 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
+++ Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
@@ -9,6 +9,7 @@ sorted_letters = sorted(letters, reverse = True)
 animals = ['elephant', 'cat', 'moose', 'antelope', 'elk', 'rabbit', 'zebra', 'yak', 'salamander', 'deer', 'otter', 'minx', 'giraffe', 'goat', 'cow', 'tiger', 'bear']
 animals_sorted = sorted(animals)
 
+
 #The dictionary, medals, shows the medal count for six countries during the Rio Olympics. Sort the country names so they appear alphabetically. Save this list to 
 #the variable alphabetical.
 medals = {'Japan':41, 'Russia':56, 'South Korea':21, 'United States':121, 'Germany':42, 'China':70}

fa866a8798ac7ea779ec8ce65c9bd80fc5fb9389
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-22 23:10:56 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_6.py
+++ Python_Functions,_Files,_and_Dictionaries/week_4/course_2_assessment_6.py
@@ -2,7 +2,6 @@
 
 #Write a function, sublist, that takes in a list of numbers as the parameter. In the function, use a while loop to return a sublist of the input list. The sublist 
 #should contain the same values of the original list up until it reaches the number 5 (it should not contain the number 5).
-
 def sublist(x):
     sub = []
     x = (num for num in x) 

cc4b0fde1705da6fe071d379edde193a275a166a
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-22 23:09:57 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
+++ Python_Functions,_Files,_and_Dictionaries/week_5/course_2_assessment_8.py
@@ -34,6 +34,7 @@ def g(k,d):
 ks = groceries.keys()
 most_needed = sorted(ks, key=lambda x:g(x,groceries), reverse = True)
 
+
 #Create a function called last_four that takes in an ID number and returns the last four digits. For example, the number 17573005 should return 3005. Then, use this
 #function to sort the list of ids stored in the variable, ids, from lowest to highest. Save this sorted list in the variable, sorted_ids. Hint: Remember that only 
 #strings can be indexed, so conversions may be needed.

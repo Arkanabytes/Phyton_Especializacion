0924f26dc4f7371898d730f4efaee81fe7a9a523
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-23 23:21:04 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_3/course_2_assessment_5.py
+++ Python_Functions,_Files,_and_Dictionaries/week_3/course_2_assessment_5.py
@@ -3,6 +3,7 @@
 #Create a tuple called olympics with four elements: “Beijing”, “London”, “Rio”, “Tokyo”.
 olympics = ("Beijing","London","Rio","Tokyo")
 
+
 #The list below, tuples_lst, is a list of tuples. Create a list of the second elements of each tuple and assign this list to the variable country.
 tuples_lst = [('Beijing', 'China', 2008), ('London', 'England', 2012), ('Rio', 'Brazil', 2016, 'Current'), ('Tokyo', 'Japan', 2020, 'Future')]
 country = []

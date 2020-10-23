258f51f796a87e08a97be9ae0440219c9d269c4e
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-23 23:23:27 +0200

    .

--- Python_Functions,_Files,_and_Dictionaries/week_3/course_2_assessment_4.py
+++ Python_Functions,_Files,_and_Dictionaries/week_3/course_2_assessment_4.py
@@ -36,7 +36,6 @@ def length(num):
 length([4, 4, 4, 3, 5, 6, 7, 8, 9]) 
 
 
-
 #You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide function within the sum function. Do not worry about decimals.
 def divide(num):
     x = num/2
